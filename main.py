48eaf375feae0e5c60714384e76274823e9866c9
Amitgitup <amitksingh3022@gmail.com>
2025-08-19 23:40:07 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
 import sys
 
+ 
 if __name__ == "__main__":
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
@@ -24,8 +27,14 @@ if __name__ == "__main__":
         logging.info("Data Validation Completed")
         print(datavalidationartifact)
 
-        
+        datatransformationconfig = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(datavalidationartifact, datatransformationconfig)
+        logging.info("Initiate the data transformation")
+        datatransformationartifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(datatransformationartifact) 
 
+        
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -37,6 +37,19 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILENAME: str = "preprocessing.pkl"
 
 
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_VAR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
+## KNN Imputer to replace NAN Values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
