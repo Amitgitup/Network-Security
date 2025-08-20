3443fbb3a95665d2c8f5cbc2871f3ddfdc440cd9
Amitgitup <amitksingh3022@gmail.com>
2025-08-21 02:37:45 +0530

    Trainer Evaluation MLFlow

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.model_trainer import ModelTrainer
 
 import sys
 
@@ -35,6 +36,12 @@ if __name__ == "__main__":
         print(datatransformationartifact) 
 
         
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config = model_trainer_config, data_transformation_artifact = datatransformationartifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training Artifact Created")
+
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
